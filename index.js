4936eda021e474dedf4071ebe4559138fba1b209
Amman Nega <ammannega@gmail.com>
2023-04-19 22:34:17 -0700

    prompted user for inputs

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is your GitHub username? ",
+            name: "github",
+        },
+        {
+            type: "input",
+            message: "What is your email? " ,
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "Enter a project title: ",
+            name: "title",
+        },
+        {
+            type: "input",
+            message: "Write a short description of your project ",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "What command should be run to install dependencies? ",
+            name: "installation",
+        },
+        {
+            type: "input",
+            message: "What does the user need to know about using this repo? ",
+            name: "usage",
+        },
+        {
+            type: "list",
+            message: "What kind of license should your project have? ",
+            name: "license",
+            choices: ["MIT", "ISC", "Apache"],
+        },
+        {
+            type: "input",
+            message: "What should the user know about contributing to the repo? ",
+            name: "contribution",
+        },
+        {
+            type: "input",
+            message: "What command should be run to run tests? ",
+            name: "tests",
+        },
+])
